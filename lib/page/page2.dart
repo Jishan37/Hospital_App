da4f549e305d9b63575a6c7af00c2a49eabc5bbb
KAFI <kaficsebaiust02@gmail.com>
2022-01-15 21:11:47 +0600

    kafi

--- lib/page/page2.dart
+++ lib/page/page2.dart
@@ -28,257 +28,260 @@ class _TwoState extends State<Two> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey.shade300,
-        title: Center(
-          child: Text('HOSPITO',
-            style: TextStyle(
-                fontSize: 40,
-                fontFamily: 'jas',
-                fontWeight: FontWeight.bold,
-                color: Colors.red
-            ),
-          )
-        ),
-        leading: Builder(
-          builder: (context){
-            return IconButton(onPressed: ()=>Scaffold.of(context).openDrawer(),
-            icon: Card(
-              color: Colors.black54,
-              child: Icon(MdiIcons.segment,
-                size: 32,
-              ),
-            ),
-            );
-          },
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 5),
-            child: Builder(
-                builder: (context){
-                  return IconButton(onPressed: ()=>Scaffold.of(context).openEndDrawer(),
-                      icon: Icon(Icons.search,
-                        color: Colors.black54,
-                        size: 40,
-                      ));
-                }
-            ),
-          )
-        ],
-      ),
-      drawer: Drawer(
-        child: Container(
-          color: Colors.red.shade400,
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: ListView(
-              children: [
-                Container(
-                  /*child: UserAccountsDrawerHeader(
-                    currentAccountPictureSize: Size.fromRadius(45.0),
-                    currentAccountPicture: CircleAvatar(
-                      backgroundImage: AssetImage('assets/44.png'),
-                    ),
-                    accountName: Text(''),
-                    accountEmail: Text(''),
-                    arrowColor: Colors.green.shade100,
-
-                  ),*/
-                  height: 160,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.fitWidth,
-                          image: AssetImage("assets/home6.jpg")),
-                      borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),topLeft: Radius.circular(20))
-                  ),
-                ),
-                SizedBox(height: 7,),
-                Card(
-                  child: ListTile(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (cnx)=>HospitalMainPage()));
-                    },
-                    leading: Icon(MdiIcons.hospitalBuilding,size: 30,color: Colors.green,),
-                    title: Text('হাসপাতাল',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    onTap: (){
-                       Navigator.push(context, MaterialPageRoute(builder: (cnx)=>DeaseasePage()));
-                    },
-                    leading: Icon(MdiIcons.doctor,size: 30,color: Colors.green),
-                    title: Text('রোগ বিশেষজ্ঞ',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    onTap: (){
-                       Navigator.push(context, MaterialPageRoute(builder: (cnx)=>DiagnosticPage()));
-                    },
-                    leading: Icon(MdiIcons.officeBuilding,size: 32,color: Colors.green,),
-                    title: Text('ডায়াগনস্টিক সেন্টার',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    onTap: (){
-                      // Navigator.push(context, MaterialPageRoute(builder: (cnx)=>Hospital_page()));
-                    },
-                    leading: Icon(MdiIcons.ambulance,size: 30,color: Colors.green),
-                    title: Text('অ্যাম্বুলেন্স সার্ভিস',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    onTap: (){
-                      // Navigator.push(context, MaterialPageRoute(builder: (cnx)=>Hospital_page()));
-                    },
-                    leading: Icon(MdiIcons.textBox,size: 30,color: Colors.green),
-                    title: Text('পরামর্শ',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    onTap: (){
-                       Navigator.push(context, MaterialPageRoute(builder: (cnx)=>BMI_Page()));
-                    },
-                    leading: Icon(MdiIcons.calculatorVariantOutline,size: 30,color: Colors.green),
-                    title: Text('বিএমআই(BMI)',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
-                  ),
-                ),
-                Container(
-                  child: Divider(
-                    height: 30,
-                    thickness: 5,
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    onTap: (){
-                      // Navigator.push(context, MaterialPageRoute(builder: (cnx)=>Hospital_page()));
-                    },
-                    leading: Icon(Icons.account_box_outlined,size: 30,color: Colors.black87),
-                    title: Text('About',style: TextStyle(fontSize: 20,color: Colors.black87,fontWeight: FontWeight.bold),),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    onTap: (){
-                      // Navigator.push(context, MaterialPageRoute(builder: (cnx)=>Hospital_page()));
-                    },
-                    leading: Icon(Icons.question_answer_outlined,size: 30,color: Colors.black87),
-                    title: Text('Faq',style: TextStyle(fontSize: 20,color: Colors.black87,fontWeight: FontWeight.bold),),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      endDrawer: Drawer(
-        child: Align(
-          alignment: Alignment(.1,-.8),
-          child: Padding(
-            padding: const EdgeInsets.all(13),
-            child: Container(
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText:'Search',
-                    hintStyle: TextStyle(
-                    fontSize: 20,
-                ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20)
-                  )
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavyBar(
-        backgroundColor: Colors.grey.shade300,
-        selectedIndex: CurrentIndex,
-        animationDuration: Duration(milliseconds: 200),
-        //itemCornerRadius: 500,
-        containerHeight: 60,
-        //showElevation: true,
-        //iconSize: 40,
-        onItemSelected: (index){
-          setState(() {
-            CurrentIndex= index;
-          });
-        },
-        items: [
-          BottomNavyBarItem(
-              icon: Icon(MdiIcons.hospitalBuilding,
-              size: 28,
-              ),
-              title: Text('Hospital',
+    return AspectRatio(
+      aspectRatio: 16/9,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey.shade300,
+          title: Center(
+            child: Text('HOSPITO',
               style: TextStyle(
-                fontSize: 15.5,
-                fontWeight: FontWeight.bold
+                  fontSize: 40,
+                  fontFamily: 'jas',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red
               ),
-              ),
-              activeColor: Colors.red,
-              inactiveColor: Colors.blueAccent
+            )
           ),
-          BottomNavyBarItem(
-              icon: Icon(MdiIcons.skull,
-                size: 28,
-              ),
-              title: Text('Disease',
-                style: TextStyle(
-                    fontSize: 15.5
+          leading: Builder(
+            builder: (context){
+              return IconButton(onPressed: ()=>Scaffold.of(context).openDrawer(),
+              icon: Card(
+                color: Colors.black54,
+                child: Icon(MdiIcons.segment,
+                  size: 32,
                 ),
               ),
-              activeColor: Colors.red,
-              inactiveColor: Colors.red
+              );
+            },
           ),
-          BottomNavyBarItem(
-              icon: Icon(MdiIcons.homeCircle,
-                size: 28,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 5),
+              child: Builder(
+                  builder: (context){
+                    return IconButton(onPressed: ()=>Scaffold.of(context).openEndDrawer(),
+                        icon: Icon(Icons.search,
+                          color: Colors.black54,
+                          size: 40,
+                        ));
+                  }
               ),
-              title: Text('Home',
-                style: TextStyle(
-                    fontSize: 15.5
+            )
+          ],
+        ),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.red.shade400,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: ListView(
+                children: [
+                  Container(
+                    /*child: UserAccountsDrawerHeader(
+                      currentAccountPictureSize: Size.fromRadius(45.0),
+                      currentAccountPicture: CircleAvatar(
+                        backgroundImage: AssetImage('assets/44.png'),
+                      ),
+                      accountName: Text(''),
+                      accountEmail: Text(''),
+                      arrowColor: Colors.green.shade100,
+
+                    ),*/
+                    height: 160,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.fitWidth,
+                            image: AssetImage("assets/home6.jpg")),
+                        borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),topLeft: Radius.circular(20))
+                    ),
+                  ),
+                  SizedBox(height: 7,),
+                  Card(
+                    child: ListTile(
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (cnx)=>HospitalMainPage()));
+                      },
+                      leading: Icon(MdiIcons.hospitalBuilding,size: 30,color: Colors.green,),
+                      title: Text('হাসপাতাল',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      onTap: (){
+                         Navigator.push(context, MaterialPageRoute(builder: (cnx)=>DeaseasePage()));
+                      },
+                      leading: Icon(MdiIcons.doctor,size: 30,color: Colors.green),
+                      title: Text('রোগ বিশেষজ্ঞ',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      onTap: (){
+                         Navigator.push(context, MaterialPageRoute(builder: (cnx)=>DiagnosticPage()));
+                      },
+                      leading: Icon(MdiIcons.officeBuilding,size: 32,color: Colors.green,),
+                      title: Text('ডায়াগনস্টিক সেন্টার',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      onTap: (){
+                        // Navigator.push(context, MaterialPageRoute(builder: (cnx)=>Hospital_page()));
+                      },
+                      leading: Icon(MdiIcons.ambulance,size: 30,color: Colors.green),
+                      title: Text('অ্যাম্বুলেন্স সার্ভিস',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      onTap: (){
+                        // Navigator.push(context, MaterialPageRoute(builder: (cnx)=>Hospital_page()));
+                      },
+                      leading: Icon(MdiIcons.textBox,size: 30,color: Colors.green),
+                      title: Text('পরামর্শ',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      onTap: (){
+                         Navigator.push(context, MaterialPageRoute(builder: (cnx)=>BMI_Page()));
+                      },
+                      leading: Icon(MdiIcons.calculatorVariantOutline,size: 30,color: Colors.green),
+                      title: Text('বিএমআই(BMI)',style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                  Container(
+                    child: Divider(
+                      height: 30,
+                      thickness: 5,
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      onTap: (){
+                        // Navigator.push(context, MaterialPageRoute(builder: (cnx)=>Hospital_page()));
+                      },
+                      leading: Icon(Icons.account_box_outlined,size: 30,color: Colors.black87),
+                      title: Text('About',style: TextStyle(fontSize: 20,color: Colors.black87,fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      onTap: (){
+                        // Navigator.push(context, MaterialPageRoute(builder: (cnx)=>Hospital_page()));
+                      },
+                      leading: Icon(Icons.question_answer_outlined,size: 30,color: Colors.black87),
+                      title: Text('Faq',style: TextStyle(fontSize: 20,color: Colors.black87,fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        endDrawer: Drawer(
+          child: Align(
+            alignment: Alignment(.1,-.8),
+            child: Padding(
+              padding: const EdgeInsets.all(13),
+              child: Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText:'Search',
+                      hintStyle: TextStyle(
+                      fontSize: 20,
+                  ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    )
+                  ),
                 ),
               ),
-              activeColor: Colors.green,
-              inactiveColor: Colors.red
+            ),
           ),
-          BottomNavyBarItem(
-              icon: Icon(MdiIcons.officeBuildingOutline,
+        ),
+        bottomNavigationBar: BottomNavyBar(
+          backgroundColor: Colors.grey.shade300,
+          selectedIndex: CurrentIndex,
+          animationDuration: Duration(milliseconds: 200),
+          //itemCornerRadius: 500,
+          containerHeight: 60,
+          //showElevation: true,
+          //iconSize: 40,
+          onItemSelected: (index){
+            setState(() {
+              CurrentIndex= index;
+            });
+          },
+          items: [
+            BottomNavyBarItem(
+                icon: Icon(MdiIcons.hospitalBuilding,
                 size: 28,
-              ),
-              title: Text('Diagnostic',
-                style: TextStyle(
-                    fontSize: 15.5
                 ),
-              ),
-              activeColor: Colors.orange,
-              inactiveColor: Colors.red
-          ),
-          BottomNavyBarItem(
-              icon: Icon(MdiIcons.ambulance,
-                size: 28,
-              ),
-              title: Text('Ambulance',
+                title: Text('Hospital',
                 style: TextStyle(
-                    fontSize: 14,
-                  fontWeight: FontWeight.w700
+                  fontSize: 15.5,
+                  fontWeight: FontWeight.bold
                 ),
-              ),
-              activeColor: Colors.green,
-              inactiveColor: Colors.red
-          ),
-        ],
+                ),
+                activeColor: Colors.red,
+                inactiveColor: Colors.blueAccent
+            ),
+            BottomNavyBarItem(
+                icon: Icon(MdiIcons.skull,
+                  size: 28,
+                ),
+                title: Text('Disease',
+                  style: TextStyle(
+                      fontSize: 15.5
+                  ),
+                ),
+                activeColor: Colors.red,
+                inactiveColor: Colors.red
+            ),
+            BottomNavyBarItem(
+                icon: Icon(MdiIcons.homeCircle,
+                  size: 28,
+                ),
+                title: Text('Home',
+                  style: TextStyle(
+                      fontSize: 15.5
+                  ),
+                ),
+                activeColor: Colors.green,
+                inactiveColor: Colors.red
+            ),
+            BottomNavyBarItem(
+                icon: Icon(MdiIcons.officeBuildingOutline,
+                  size: 28,
+                ),
+                title: Text('Diagnostic',
+                  style: TextStyle(
+                      fontSize: 15.5
+                  ),
+                ),
+                activeColor: Colors.orange,
+                inactiveColor: Colors.red
+            ),
+            BottomNavyBarItem(
+                icon: Icon(MdiIcons.ambulance,
+                  size: 28,
+                ),
+                title: Text('Ambulance',
+                  style: TextStyle(
+                      fontSize: 14,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                activeColor: Colors.green,
+                inactiveColor: Colors.red
+            ),
+          ],
+        ),
+        body: Center(
+          child: screen[CurrentIndex],),
       ),
-      body: Center(
-        child: screen[CurrentIndex],),
     );
   }
 }
