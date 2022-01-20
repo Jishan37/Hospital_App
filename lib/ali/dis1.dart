b4e742721f19a92dde54c5a8b27001d43ac35674
User <alicsebaiust01@gmail.com>
2022-01-21 02:59:56 +0600

    ali

--- lib/ali/dis1.dart
+++ lib/ali/dis1.dart
@@ -14,44 +14,69 @@ class _Dis1State extends State<Dis1> {
       appBar: AppBar(title: Text('মেডিসিন বিশেষজ্ঞ'),),
       body: ListView(
         children: [
-          SizedBox(height: 10),
-          Card(
-            // color: Colors.white60,
-            child: ListTile(
-              title: Text('ডাঃ শাহ ইমরান',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.redAccent,
+            SizedBox(height: 10),
+              Card(
+                // color: Colors.white60,
+                child: ListTile(
+                  title: Text('ডাঃ শাহ ইমরান',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                  subtitle: Text('ফোন: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.greenAccent,
+                    ),
+                  ),
                 ),
               ),
-              subtitle: Text('ফোন: ',
+            Card(
+              child: ListTile(
+                title: Text('ডাঃ মোঃ এনামুল হক',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                subtitle: Text('ফোন: ',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.greenAccent,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('সহকারী অধ্যাপক ডাঃ অঙ্কুর দত্ত',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                subtitle: Text('ফোন: 01787-100087',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.greenAccent,
+                  ),
+                ),
+              ),
+            ),
+           /* ElevatedButton(onPressed: (){},
+                child: Text('ফোন: 01787-100087',
                 style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
                   color: Colors.greenAccent,
-                ),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('ডাঃ মোঃ এনামুল হক',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red,
-                ),
-              ),
-              subtitle: Text('ফোন: ',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.greenAccent,
-                ),
-              ),
-            ),
-          ),
+                ),),),*/
         ],
       ),
     );
