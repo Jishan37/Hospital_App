086b51921c0094cdbe7d346f074a7b753ef04b43
WALTON <mdjas005@gmail.com>
2022-01-24 13:42:46 +0600

    gdffg

--- lib/ali/dis1.dart
+++ lib/ali/dis1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Dis1 extends StatefulWidget {
   const Dis1({Key? key}) : super(key: key);
@@ -14,69 +15,49 @@ class _Dis1State extends State<Dis1> {
       appBar: AppBar(title: Text('মেডিসিন বিশেষজ্ঞ'),),
       body: ListView(
         children: [
-            SizedBox(height: 10),
-              Card(
-                // color: Colors.white60,
-                child: ListTile(
-                  title: Text('ডাঃ শাহ ইমরান',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.redAccent,
-                    ),
-                  ),
-                  subtitle: Text('ফোন: ',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.greenAccent,
-                    ),
-                  ),
-                ),
-              ),
-            Card(
-              child: ListTile(
-                title: Text('ডাঃ মোঃ এনামুল হক',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red,
-                  ),
-                ),
-                subtitle: Text('ফোন: ',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.greenAccent,
-                  ),
-                ),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text('সহকারী অধ্যাপক ডাঃ অঙ্কুর দত্ত',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red,
-                  ),
-                ),
-                subtitle: Text('ফোন: 01787-100087',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.greenAccent,
-                  ),
-                ),
-              ),
-            ),
-           /* ElevatedButton(onPressed: (){},
-                child: Text('ফোন: 01787-100087',
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:');
+            },
+              child: Text('ডাঃ শাহ ইমরান\n'
+                  '        ফোন: ',
                 style: TextStyle(
-                  fontSize: 16,
+                  fontSize: 20,
                   fontWeight: FontWeight.bold,
-                  color: Colors.greenAccent,
-                ),),),*/
+                  color: Colors.red,
+                ),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:');
+            },
+              child: Text('ডাঃ মোঃ এনামুল হক\n'
+                  '        ফোন: ',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),),
+            ),
+          ),
+           Padding(
+             padding: const EdgeInsets.all(10),
+             child: ElevatedButton(onPressed: (){
+               launch('tel:01787-100087');
+             },
+               child: Text('সহকারী অধ্যাপক ডাঃ অঙ্কুর দত্ত\n'
+                   '        ফোন: 01787-100087',
+                 style: TextStyle(
+                   fontSize: 20,
+                   fontWeight: FontWeight.bold,
+                   color: Colors.red,
+                 ),),
+                  ),
+           ),
         ],
       ),
     );
