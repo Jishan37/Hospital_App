c3d2b8ff9ec8b808465d03a30740a7f85e65543c
User <alicsebaiust01@gmail.com>
2022-01-25 13:17:48 +0600

    ali

--- lib/ali/dis12.dart
+++ lib/ali/dis12.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Dis12 extends StatefulWidget {
   const Dis12({Key? key}) : super(key: key);
@@ -11,47 +12,139 @@ class _Dis12State extends State<Dis12> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('স্ত্রী, প্রসূতি বিদ্যা ও বন্ধ্যা রোগ বিশেষজ্ঞ'),),
+      appBar: AppBar(
+        backgroundColor: Colors.purple.shade100,
+        title: Text('স্ত্রী, প্রসূতি বিদ্যা ও বন্ধ্যা রোগ বিশেষজ্ঞ'),),
       body: ListView(
         children: [
-          SizedBox(height: 10),
-          Card(
-            // color: Colors.white60,
-            child: ListTile(
-              title: Text('ডাঃ লিপি পাল',
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:01716-661428');
+            },
+              child: Text('ডাঃ লিপি পাল\n'
+                  '        ফোন: 01716-661428',
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
-                  color: Colors.redAccent,
-                ),
-              ),
-              subtitle: Text('ফোন: ',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.greenAccent,
-                ),
-              ),
+                  color: Colors.black87,
+                ),),
             ),
           ),
-          Card(
-            child: ListTile(
-              title: Text('ডাঃ পারভীন মুজিব',
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:01784-38773');
+            },
+              child: Text('ডাঃ শাকিলা নার্গিস\n'
+                  '        ফোন: 01784-38773',
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 20,
                   fontWeight: FontWeight.bold,
-                  color: Colors.red,
-                ),
-              ),
-              subtitle: Text('ফোন: ',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.greenAccent,
-                ),
-              ),
+                  color: Colors.black87,
+                ),),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:01930-816847');
+            },
+              child: Text('ডাঃ লুবনা ইয়াসমিন\n'
+                  '        ফোন: 01930-816847',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:01730-087949');
+            },
+              child: Text('কর্নেল ডাঃ শাহনাজ আক্তার\n'
+                  '        ফোন: 01730-087949',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:01762-269616');
+            },
+              child: Text('ডাঃ দিলরুবা আক্তার\n'
+                  '        ফোন: 01762-269616',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:01711-144786');
+            },
+              child: Text('ডাঃ নাসিমা আক্তার\n'
+                  '        ফোন: 01711-144786',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:01711-797904');
+            },
+              child: Text('ডাঃ রায়হানা সুলতানা বেগম\n'
+                  '        ফোন: 01711-797904',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:01772-797904');
+            },
+              child: Text('ডাঃ শান্তনা রানী পাল\n'
+                  '        ফোন: 01772-797904',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){
+              launch('tel:01772-581919');
+            },
+              child: Text('ডাঃ মৃণালিনী শর্মা\n'
+                  '        ফোন: 01772-581919',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),),
+            ),
+          ),
+
         ],
       ),
     );
